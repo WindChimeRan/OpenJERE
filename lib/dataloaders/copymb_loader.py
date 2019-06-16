a0cbf58e8a7b0b3b0f51ab563b69bbb32c81ceb8
WindChimeRan <1012869439@qq.com>
2019-06-16 18:14:35 +0800

    to loader

--- lib/dataloaders/copymb_loader.py
+++ lib/dataloaders/copymb_loader.py
@@ -12,3 +12,101 @@ from functools import partial
 from typing import Dict, List, Tuple, Set, Optional
 
 from .abc_dataset import Abstract_dataset
+
+
+class Copymb_Dataset(Abstract_dataset):
+    def __init__(self, hyper, dataset):
+
+        super(Copymb_Dataset, self).__init__(hyper, dataset)
+
+        self.seq_list = []
+        self.text_list = []
+        self.bio_list = []
+        self.spo_list = []
+
+        for line in open(os.path.join(self.data_root, dataset), 'r'):
+            line = line.strip("\n")
+            instance = json.loads(line)
+
+            self.seq_list.append(instance['seq'])
+            self.text_list.append(instance['text'])
+            self.bio_list.append(instance['bio'])
+            self.spo_list.append(instance['spo_list'])
+
+    def __getitem__(self, index):
+
+        seq = self.seq_list[index]
+        text = self.text_list[index]
+        bio = self.bio_list[index]
+        spo = self.spo_list[index]
+
+        tokens_id = self.text2tensor(text)
+        bio_id = self.bio2tensor(bio)
+        seq_id = self.seq2tensor(text, seq)
+
+        return tokens_id, bio_id, seq_id, len(text), spo, text, bio
+
+    def __len__(self):
+        return len(self.text_list)
+
+    def text2tensor(self, text: List[str]) -> torch.tensor:
+        oov = self.word_vocab['oov']
+        padded_list = list(map(lambda x: self.word_vocab.get(x, oov), text))
+        padded_list.extend([self.word_vocab['<pad>']] *
+                           (self.hyper.max_text_len - len(text)))
+        return torch.tensor(padded_list)
+
+    def bio2tensor(self, bio):
+        # here we pad bio with "O". Then, in our model, we will mask this "O" padding.
+        # in multi-head selection, we will use "<pad>" token embedding instead.
+        padded_list = list(map(lambda x: self.bio_vocab[x], bio))
+        padded_list.extend([self.bio_vocab['O']] *
+                           (self.hyper.max_text_len - len(bio)))
+        return torch.tensor(padded_list)
+
+    def seq2tensor(self, text, selection):
+        # s p o
+        result = torch.zeros(
+            (self.hyper.max_text_len, self.hyper.max_decode_len * 2 + 1))
+        NA = self.relation_vocab['N']
+        result[:, NA, :] = 1
+        for triplet in selection:
+
+            object = triplet['object']
+            subject = triplet['subject']
+            predicate = triplet['predicate']
+
+            result[subject, predicate, object] = 1
+            result[subject, NA, object] = 0
+
+        return result
+
+
+class Batch_reader(object):
+    def __init__(self, data):
+        transposed_data = list(zip(*data))
+        # tokens_id, bio_id, selection_id, spo, text, bio
+
+        self.tokens_id = pad_sequence(transposed_data[0], batch_first=True)
+        self.bio_id = pad_sequence(transposed_data[1], batch_first=True)
+        self.selection_id = torch.stack(transposed_data[2], 0)
+
+        self.length = transposed_data[3]
+
+        self.spo_gold = transposed_data[4]
+        self.text = transposed_data[5]
+        self.bio = transposed_data[6]
+
+    def pin_memory(self):
+        self.tokens_id = self.tokens_id.pin_memory()
+        self.bio_id = self.bio_id.pin_memory()
+        self.selection_id = self.selection_id.pin_memory()
+        return self
+
+
+def collate_fn(batch):
+    return Batch_reader(batch)
+
+
+Copymb_Dataset_loader = partial(
+    DataLoader, collate_fn=collate_fn, pin_memory=True)
