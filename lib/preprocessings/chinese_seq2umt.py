93d37b5c0015e551d1133c59bca14cb5b6619b1b
WindChimeRan <1012869439@qq.com>
2020-02-08 22:29:28 -0600

    todo: seq2umt

--- lib/preprocessings/chinese_seq2umt.py
+++ lib/preprocessings/chinese_seq2umt.py
@@ -47,7 +47,7 @@ class Chinese_selection_preprocessing(Chinese):
         }
         return json.dumps(result, ensure_ascii=False)
 
-    def _train_read_line(self, line: str) -> Optional[str]:
+    def _train_read_line(self, line: str) -> List[str]:
         # teacher forcing
         # batches are aligned by the triplets rather than sentences.
         line = line.strip("\n")
@@ -70,16 +70,12 @@ class Chinese_selection_preprocessing(Chinese):
                 for spo in spo_list
             ]
 
-            entities: List[str] = self.spo_to_entities(text, spo_list)
-            relations: List[str] = self.spo_to_relations(text, spo_list)
+        result = [json.dumps({"text": text, "spo_list": [spo]}, ensure_ascii=False) for spo in spo_list]
+        return result
 
-        result = {
-            "text": text,
-            "spo_list": spo_list,
-        }
-        return json.dumps(result, ensure_ascii=False)
     @overrides
     def _check_valid(self, text: str, spo_list: List[Dict[str, str]]) -> bool:
+        # TODO: ???
         # if spo_list == []:
         #     return False
         # if len(text) > self.hyper.max_text_len:
@@ -98,7 +94,17 @@ class Chinese_selection_preprocessing(Chinese):
     @overrides
     def gen_all_data(self):
         print("Override method: Different formats of train/dev!")
-        pass
+        self.gen_train_data()
+        # TODO
 
     def gen_train_data(self):
-        pass
+        train = self.hyper.train
+        source = os.path.join(self.raw_data_root, train)
+        target = os.path.join(self.data_root, train)
+        with open(source, "r") as s, open(target, "w") as t:
+            for line in s:
+                newlines = self._read_line(line)
+                if newlines is not None:
+                    for newline in newlines:
+                        t.write(newline)
+                        t.write("\n")
