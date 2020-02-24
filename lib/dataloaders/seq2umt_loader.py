821cab6f8745221b7f5d01e5dea5a42f4e9ab3d0
yiyunya <yiyunya@foxmail.com>
2020-02-24 19:05:44 +0900

    update utf encoding again and again

--- lib/dataloaders/seq2umt_loader.py
+++ lib/dataloaders/seq2umt_loader.py
@@ -80,7 +80,7 @@ class Seq2umt_Dataset(Abstract_dataset):
         #     self.R_in,
         # ) = ([] for _ in range(7))
 
-        for line in open(os.path.join(self.data_root, dataset), "r"):
+        for line in open(os.path.join(self.data_root, dataset), "r", encoding='utf-8'):
             line = line.strip("\n")
             instance = json.loads(line)
 
