f92c6d897776a1ba09a9b8167387f8bb4836c253
WindChimeRan <1012869439@qq.com>
2020-03-14 17:35:49 -0500

    refactor

--- lib/preprocessings/__init__.py
+++ lib/preprocessings/__init__.py
@@ -2,5 +2,3 @@ from lib.preprocessings.selection import *
 from lib.preprocessings.copymb import *
 from lib.preprocessings.two_tagging import *
 from lib.preprocessings.seq2umt import *
-
-# from lib.preprocessings.preprocessor import Chinese_seq2umt_preprocessing
