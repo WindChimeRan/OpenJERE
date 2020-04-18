27466482289d1d693f3f6696f3104fdfcbf87059
WindChimeRan <1012869439@qq.com>
2020-04-18 17:47:02 -0500

    add drawing

---
+++ image/mutli_triplet_nyt.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+
+a = """
+100%|███████████████████████████████████████████████████████████████| 25/25 [00:39<00:00,  1.41s/it]
+precision: 0.5778, recall: 0.5677, fscore: 0.5727 ||
+f1 =  0.572731932342393
+100%|███████████████████████████████████████████████████████████████| 18/18 [00:40<00:00,  2.00s/it]
+precision: 0.7631, recall: 0.7236, fscore: 0.7428 ||
+f1 =  0.7428111509475398
+100%|█████████████████████████████████████████████████████████████████| 6/6 [00:18<00:00,  2.98s/it]
+precision: 0.7498, recall: 0.6658, fscore: 0.7053 ||
+f1 =  0.7053002473948616
+100%|█████████████████████████████████████████████████████████████████| 3/3 [00:09<00:00,  3.10s/it]
+precision: 0.7992, recall: 0.7039, fscore: 0.7485 ||
+f1 =  0.7484960666358226
+100%|█████████████████████████████████████████████████████████████████| 4/4 [00:18<00:00,  4.28s/it]
+precision: 0.8380, recall: 0.7386, fscore: 0.7852 ||
+f1 =  0.7851783598789024
+"""
+
+
+wdec = [0.4930, 0.3622, 0.3504]
+seq2umtree = [0.3708, 0.2679, 0.3203]
+
+# print(a.splitlines())
+# result = []
+# for line in a.splitlines():
+#     if 'fscore' in line:
+#         # print(line)
+#         score = float(line[-10:-2])
+#         result.append(score)
+#         # print(score)
+# print(result)
+x = [1, 2, 3]
+plt.plot(x, wdec, color='g', label='WDec')
+plt.plot(x, seq2umtree, color='orange', label='Seq2UMTree')
+# plt.plot(x, selection, color='blue',  label='Selection')
+
+plt.xlabel('Triplet number per sentence')
+plt.ylabel('F1')
+plt.ylim(0, 1)
+# plt.xlim(0, 1)
+plt.xticks(range(1,4), ["1", "2", ">2"])
+
+plt.legend()
+# plt.title('DuIE')
+plt.show()
--- split_recitatiton.py
+++ split_recitatiton.py
@@ -31,6 +31,7 @@ def cnt_train(data_root):
 
 
 def filter_test(data_root, dataset, cnt, thr: int):
+    # filter the sub test set whose freq less than thr
     source = os.path.join(data_root, dataset)
     target = os.path.join(data_root, "filter_" + str(thr) + "_" + dataset)
     write_linenum = 0
@@ -40,14 +41,38 @@ def filter_test(data_root, dataset, cnt, thr: int):
         for all_linenum, line in enumerate(s):
             jline = json.loads(line)
             spo_list = jline["spo_list"]
-            if check_valid(spo_list, cnt, thr):
+            if check_thr(spo_list, cnt, thr):
                 t.write(line)
                 write_linenum += 1
     print(data_root + " valid sent / all sent = %d/%d" % (write_linenum, all_linenum))
     return write_linenum / all_linenum
 
 
-def check_valid(spo_list, cnt, MAX):
+def filter_test_num(data_root, dataset, cnt, thr: int, num: int, fn):
+    # filter the sub test set whose freq less than thr
+    source = os.path.join(data_root, dataset)
+    target = os.path.join(data_root, "new" + str(thr) + "_" + str(num) + "_" + dataset)
+    write_linenum = 0
+    with open(source, "r", encoding="utf-8") as s, open(
+        target, "w", encoding="utf-8"
+    ) as t:
+        for all_linenum, line in enumerate(s):
+            jline = json.loads(line)
+            spo_list = jline["spo_list"]
+            if check_thr(spo_list, cnt, thr) and check_num(spo_list, fn):
+                t.write(line)
+                write_linenum += 1
+    print(data_root + " valid sent / all sent = %d/%d" % (write_linenum, all_linenum))
+    return write_linenum / all_linenum
+
+
+def check_num(spo_list, fn):
+    # MAX = 3
+    spo = [(t["subject"], t["predicate"], t["object"]) for t in spo_list]
+    return fn(len(spo))
+
+
+def check_thr(spo_list, cnt, MAX):
     # MAX = 3
     spo = [(t["subject"], t["predicate"], t["object"]) for t in spo_list]
     spo_freq = [cnt[tri] < MAX for tri in spo]
@@ -69,7 +94,24 @@ def process_all(data_root):
     filter_test(data_root, "new_test_data.json", cnt, 1)
 
 
-if __name__ == "__main__":
+def nyt_pro(data_root, cnt):
+    # cnt = cnt_train(data_root)
+    filter_test_num(
+        data_root, "new_test_data.json", cnt, 10, 1, fn=lambda length: length == 1
+    )
+    filter_test_num(
+        data_root, "new_test_data.json", cnt, 10, 2, fn=lambda length: length == 2
+    )
+    filter_test_num(
+        data_root, "new_test_data.json", cnt, 10, 3, fn=lambda length: length >= 3
+    )
 
-    for root in data_root_list:
-        process_all(root)
+
+if __name__ == "__main__":
+    nyt_list = ["data/nyt/wdec", "data/nyt/seq2umt_pos"]
+    cnt = cnt_train(nyt_list[1])
+    for root in nyt_list:
+        nyt_pro(root, cnt)
+
+    # for root in data_root_list:
+    #     process_all(root)
