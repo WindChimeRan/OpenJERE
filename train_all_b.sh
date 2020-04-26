1e7e9f5f9cac847884b92aab4a8caec540150e1e
WindChimeRan <1012869439@qq.com>
2020-04-25 22:22:33 -0500

    update all bugs

---
+++ train_all_b.sh
@@ -0,0 +1,51 @@
+exps=(
+    chinese_selection
+    chinese_wdec
+    chinese_seq2umt_ops
+    chinese_seq2umt_osp
+    chinese_seq2umt_spo
+    chinese_seq2umt_sop
+    chinese_seq2umt_pso
+    chinese_seq2umt_pos
+    nyt_selection
+    nyt_wdec
+    nyt_seq2umt_ops
+    nyt_seq2umt_osp
+    nyt_seq2umt_spo
+    nyt_seq2umt_sop
+    nyt_seq2umt_pso
+    nyt_seq2umt_pos
+)
+
+new_exps=(
+    chinese_seq2umt_ops
+    chinese_seq2umt_osp
+    chinese_seq2umt_spo
+)
+
+# nytexps=(
+#     nyt_selection
+#     nyt_wdec
+#     nyt_seq2umt_ops
+#     nyt_seq2umt_osp
+#     nyt_seq2umt_spo
+#     nyt_seq2umt_sop
+#     nyt_seq2umt_pso
+#     nyt_seq2umt_pos
+#     chinese_seq2umt_pos
+# )
+
+nytexps=(
+    nyt_seq2umt_ops
+    nyt_seq2umt_osp
+    nyt_seq2umt_spo
+    nyt_seq2umt_sop
+    nyt_seq2umt_pso
+    nyt_seq2umt_pos
+)
+
+
+for exp in "${new_exps[@]}"; do
+	python main.py -e $exp -m preprocessing
+    python main.py -e $exp -m train
+done
